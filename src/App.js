1cf6e67dd051c67c99d99285b032877e4d919576
Cole <propertyisrobbery@hotmail.com>
2023-01-21 21:33:32 -0500

    add firebase and firebase config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 function App() {
   return (
     <div className="App">
+      <div>
+        <input placeholder="Search for a beer" type="text" />
+      </div>
+
       <h1>This week's beer:</h1>
       <div className="weekly-beer-container"></div>
     </div>
