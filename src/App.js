7e0b50f0aac5f613581622c79744ede4ff9271af
Cole <propertyisrobbery@hotmail.com>
2023-01-21 20:09:16 -0500

    complete some basic setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>This week's beer:</h1>
+      <div className="weekly-beer-container"></div>
+    </div>
+  );
+}
+
+export default App;
