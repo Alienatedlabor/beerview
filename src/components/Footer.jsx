0d862c89180e418a4ce851a490cfa0e1036c25b5
Cole <propertyisrobbery@hotmail.com>
2023-02-07 20:38:32 -0500

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className=" flex justify-center text-white">
+      <a
+        href="https://github.com/Alienatedlabor"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Developed by Cole Heideman
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
