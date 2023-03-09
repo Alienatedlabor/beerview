e1d85d61336e86688b1ec57b6a08a62ec48a8b2b
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-03-09 15:13:32 -0500

    add img

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <div className="flex justify-between bg-yellow-500 px-3 py-4">
-      <img src="" alt="logoimage" />
-      <h1 className="text-center text-3xl font-bold">Brewview</h1>
-      <nav className="place-content-">
+      <img className="h-16" src="src/assets/logo.webp" alt="logoimage" />
+      <h1 className="justify-center text-center text-3xl font-bold">
+        Brewview
+      </h1>
+      <nav className="place-content">
         <Link to="/account" className="underline hover:text-white">
           Account
         </Link>
