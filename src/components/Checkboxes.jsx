83cf26ef907abb956234bd642d80471c3466b211
alienatedlabor <104693087+Alienatedlabor@users.noreply.github.com>
2023-03-09 15:14:58 -0500

    add clickhandle (not working yet)

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -5,16 +5,41 @@ import { useEffect } from 'react';
 
 const Checkboxes = () => {
   const [userList, setUserList] = useState([]);
-  const { getUserList } = UserAuth();
-  const handleClick = () => {};
+  const [checked, setChecked] = useState(false);
+  const { getUserList, user, updateUser } = UserAuth();
+  const handleCheck = (isChecked) => {
+    setChecked(isChecked);
+    console.log(isChecked);
+    //check doesnt work and also I think it's backwards- I need value of databse to determine value of isChecked, I think
+
+    if (isChecked === true) {
+      updateUser(user.uid, {
+        finishedVoting: true,
+      });
+      if (isChecked === false) {
+        updateUser(user.uid, {
+          finishedVoting: false,
+        });
+      }
+    }
+  };
+
   useEffect(() => {
+    // runs getUserList and passes the result to the state
     getUserList().then(setUserList);
   }, []);
+
   return (
     <div className="mx-1 my-4 flex justify-center gap-6 ">
-      {userList.map((user) => (
-        <div className="flex justify-center ">
-          <Entry label={user.username} type="checkbox" onChange={handleClick} />
+      {userList.map((u) => (
+        <div key={u.id} className="flex justify-center ">
+          <Entry
+            label={u.username}
+            type="checkbox"
+            onChange={(e) => handleCheck(e.target.checked)}
+            disabled={user.uid !== u.id}
+            value={checked}
+          />
         </div>
       ))}
     </div>
